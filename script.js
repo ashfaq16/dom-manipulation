a044312bc0d4d076f535f995531812713dc98eca
Ashfaq Aziz <ashfaqaziz16@gmail.com>
2018-05-30 04:16:19 +0000

    Task2a

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ var heading = document.getElementById('heading');
 console.log(heading.innerText);
 
 /* Task 2 using plain JavaScript */
-
+//Task2a
+var textBtn = document.getElementById('textBtn2');
+textBtn.addEventListener('click', function(){
+    var task2=document.getElementById('task2a');
+     var paragraph = document.createElement('p');
+     paragraph.innerText = "Hello World";
+     task2.appendChild(paragraph);
+})
 
 
 /* Task 4 using jQuery */
