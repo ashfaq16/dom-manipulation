d5ddde89b17aeb438431b3f40b42cd778f6773c3
Ashfaq Aziz <ashfaqaziz16@gmail.com>
2018-06-04 04:31:25 +0000

    Task4c

--- script.js
+++ script.js
@@ -41,3 +41,9 @@ function changeBG(event){
     var color = event.target.innerText.toLowerCase();
     $('body').css('background-color',color)
 }
+
+//Task4c
+$('#sum').click(function(event){
+    var sum = Number($('#num1').val()) + Number($('#num2').val())
+    $('#answer').text(sum)
+})
